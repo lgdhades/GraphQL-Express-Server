fb928c83fafad2bb0f971b4dfa912cc871fbbd50
lgdhades <hades987456@outlook.com>
2021-04-24 05:58:40 +0200

    feat: install CORS package to avoid CORS-Error

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const { graphqlHTTP } = require('express-graphql')
 const schema = require('./GraphQLSchema/GraphQLSchema')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const cors = require('cors')
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.dvqs8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 )
 
 mongoose.connection.once('open', () => console.log('connected to database'))
+
+app.use(cors())
 
 app.listen(5000, () => console.log('Now listening for requests on port 5000'))
 
